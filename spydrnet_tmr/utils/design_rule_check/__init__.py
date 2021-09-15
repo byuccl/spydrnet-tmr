d96726911525c77309b9fbb848c88f0d7f19bfb0
Jacob Brown <jacobb00@byu.edu>
2021-09-15 17:56:32 -0600

    added init.py file to drc folder to simplify importing

---
+++ spydrnet_tmr/utils/design_rule_check/__init__.py
@@ -0,0 +1,6 @@
+from spydrnet_tmr.utils.design_rule_check.drc_apply_nmr import check_nmr
+from spydrnet_tmr.utils.design_rule_check.drc_connections_after_replication_and_insertion import check_connections
+from spydrnet_tmr.utils.design_rule_check.drc_insert_organs import check_organs
+from spydrnet_tmr.utils.design_rule_check.drc_properties_after_replication import check_properties_after_replication
+from spydrnet_tmr.utils.design_rule_check.drc_surface_pins import check_surfaced_pins
+from spydrnet_tmr.utils.design_rule_check.drc_uniquify_nmr_property import check_uniquify_nmr_property
