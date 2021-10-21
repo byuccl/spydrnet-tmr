2ec8ac06841ee2ae1d1e1746a2d8bf97e641a977
Ben Glines <benglines1@gmail.com>
2021-10-20 17:26:37 -0700

    Rearranged functions to make main more flexible

--- spydrnet_tmr/process_config.py
+++ spydrnet_tmr/process_config.py
@@ -23,60 +23,8 @@ from spydrnet_tmr.config_constants import (
     VOTER_INSERTION,
 )
 
-from spydrnet_tmr.apply_tmr_to_netlist import apply_tmr_to_netlist
-from spydrnet_tmr.support_files.vendor_names import XILINX
 
-
-def main():
-
-    parser = argparse.ArgumentParser(description="compose netlist with TMR")
-
-    parser.add_argument(
-        "netlist_filename",
-        help="filename of the netlist from which a new netlist with TMR will be composed",
-    )
-
-    parser.add_argument(
-        "--tmr_config_yaml_filename",
-        required=False,
-        help="filename for the TMR configuration file",
-        default=None,
-    )
-
-    args = parser.parse_args()
-    netlist = sdn.parse(args.netlist_filename)
-
-    # Path name of the configuration file for applying TMR
-    if args.tmr_config_yaml_filename is None:
-        config_file_path_name = "tmr_config.yaml"
-    else:
-        config_file_path_name = args.tmr_config_yaml_filename
-
-    # Check if the config file exists
-    if os.path.isfile(config_file_path_name):
-        with open(config_file_path_name) as file:
-            tmr_config_content = yaml.load(file, Loader=FullLoader)
-            tmr_config = list(tmr_config_content.values())[0]
-            netlist = apply_tmr_to_netlist_from_config(netlist, tmr_config)
-    else:
-        # Since the config file could not be found, apply TMR with default settings
-        print(
-            config_file_path_name
-            + " could not be found. Applying TMR with default settings"
-        )
-        netlist = apply_tmr_to_netlist(
-            netlist,
-            XILINX,
-        )
-
-    netlist_filename, netlist_filename_ext = os.path.splitext(
-        args.netlist_filename
-    )
-
-    sdn.compose(netlist, netlist_filename + "_tmr" + netlist_filename_ext)
-
-
-def apply_tmr_to_netlist_from_config(netlist, tmr_config):
+def process_config(netlist, tmr_config):
     """
     apply_tmr_to_netlist_from_config
 
@@ -120,14 +68,7 @@ def apply_tmr_to_netlist_from_config(netlist, tmr_config):
         raise TMRConfigError(
             "Missing either 'voter_insertion' or 'replication' section"
         )
-
-    apply_tmr_to_netlist(
-        netlist,
-        XILINX,
-        hinstances_and_hports_to_replicate,
-        valid_voter_point_dict,
-    )
-    return netlist
+    return hinstances_and_hports_to_replicate, valid_voter_point_dict
 
 
 def create_replication_list(netlist, section):
@@ -284,7 +225,3 @@ class TMRConfigError(Exception):
     Attributes:
         message -- explanation of the error
     """
-
-
-if __name__ == "__main__":
-    main()
