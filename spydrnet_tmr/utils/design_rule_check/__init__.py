73665f0a052270a51d045d743e6917e12b5ba8dc
jacob <jacobb00@byu.edu>
2022-05-03 21:49:53 -0600

    deleted stuff causing an error

--- spydrnet_tmr/utils/design_rule_check/__init__.py
+++ spydrnet_tmr/utils/design_rule_check/__init__.py
@@ -3,8 +3,4 @@ from spydrnet_tmr.utils.design_rule_check.drc_connections_after_replication_and_
 from spydrnet_tmr.utils.design_rule_check.drc_insert_organs import check_organs
 from spydrnet_tmr.utils.design_rule_check.drc_properties_after_replication import check_properties_after_replication
 from spydrnet_tmr.utils.design_rule_check.drc_surface_pins import check_surfaced_pins
-<<<<<<< HEAD
 from spydrnet_tmr.utils.design_rule_check.drc_uniquify_nmr_property import check_uniquify_nmr_property
-=======
-from spydrnet_tmr.utils.design_rule_check.drc_uniquify_nmr_property import check_uniquify_nmr_property
->>>>>>> main
