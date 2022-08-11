8da8c5fa56ff7ccccb1b1d16273c182271eb0f25
Graeme Smecher <gsmecher@threespeedlogic.com>
2022-08-11 12:31:31 -0700

    Don't use 'is' operator with integer literals (== preferred).

--- spydrnet_tmr/transformation/replication/organ_insertion.py
+++ spydrnet_tmr/transformation/replication/organ_insertion.py
@@ -661,7 +661,7 @@ class OrganInsertion:
     def find_key(self, instance):
         start_index = instance.name.find(self.replica_suffix)
         stop_index = start_index + len(self.replica_suffix) + 2
-        if start_index is -1:
+        if start_index == -1:
             key = ""
         else:
             key = instance.name[start_index:stop_index]
